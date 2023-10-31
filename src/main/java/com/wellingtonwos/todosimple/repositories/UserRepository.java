23a2d8839a6d24b02fc7f5b8d42b08ff5eef60f9
WellingtonWoS <wellington.sousa@aluno.ifsp.edu.br>
2023-10-31 02:18:09 -0300

    Create User and Task repositories

---
+++ src/main/java/com/wellingtonwos/todosimple/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.wellingtonwos.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wellingtonwos.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    
+}
