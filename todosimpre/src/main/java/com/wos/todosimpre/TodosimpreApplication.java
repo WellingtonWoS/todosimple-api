1ca1fddbf04e58b2ddf449aabf38e3cd7a433958
Wellington Oliveira <wellington.sousa@aluno.ifsp.edu.br>
2025-02-28 01:47:47 -0300

    Projeto Spring Boot

---
+++ todosimpre/src/main/java/com/wos/todosimpre/TodosimpreApplication.java
@@ -0,0 +1,13 @@
+package com.wos.todosimpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimpreApplication.class, args);
+	}
+
+}
---
+++ todosimpre/src/test/java/com/wos/todosimpre/TodosimpreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wos.todosimpre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
